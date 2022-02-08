674a12a9b726a64129db5013ec96a5ab1872d2fa
blxssxd <iamblxssxd@gmail.com>
2022-02-07 20:18:43 -0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import { ThemeProvider, CssBaseline } from "@material-ui/core";
 import theme from "./theme";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import React from "react";
+import Home from "./scenes/Home";
+import Bookmarks from "./scenes/Bookmarks";
+import Definition from "./scenes/Definition";
 
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div></div>
+      <Router>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/bookmarks">
+          <Bookmarks />
+        </Route>
+        <Route path="/search/:word">
+          <Definition />
+        </Route>
+      </Router>
     </ThemeProvider>
   );
 };
